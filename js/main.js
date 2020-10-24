922f29a45d66b3c959b160265002746fd5dac721
Erbol Ruslanov <rslnv95@gmail.com>
2020-10-24 10:59:52 +0400

    spirit project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelector(".mobile-menu").addEventListener("click",()=>{
+    document.querySelector(".top-menu").classList.toggle("show")
+})
+document.querySelectorAll(".top-menu a").forEach((oneLink) => {
+    oneLink.addEventListener("click",() =>{
+        document.querySelector(".top-menu").classList.remove("show")
+    })
+})
